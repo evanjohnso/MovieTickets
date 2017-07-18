2cdf753c377dd0d841fbfbe449497676f668c9ef
Beth Hansen, Evan Johnson and Haneen AbuKhater <beth@>
2017-07-18 11:51:23 -0700

    add quantity functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 //Business Logic
-function MovieSpec (title, time, age) {
+function MovieSpec (title, time, age, number) {
   this.feature = title;
   this.time = time;
   this.age = age;
+  this.quantity = number;
+}
+
+MovieSpec.prototype.movieTitle = function () {
+  if (this.time === 10) {
+    return "Evening ";
+  } else if (this.time === 8){
+    return "Matinee ";
+  } else {
+    return "3D ";
+  }
 }
 
 MovieSpec.prototype.price = function() {
-  return this.time + this.age;
+  return (this.time + this.age) * this.quantity;
 }
 
 //User Interface Logic
@@ -15,15 +26,18 @@ $(document).ready(function() {
     event.preventDefault();
 
     var featureTitle = $('#features').val();
+
     var movieTime = parseInt( $("input:radio[name=time]:checked").val() );
     var userAge = parseInt( $("input:radio[name=age]:checked").val() );
+    var quantity = $('#ticketNumber').val();
 
-    var newPerson = new MovieSpec(featureTitle, movieTime, userAge);
-
-    console.log(newPerson.price() );
-    console.log(newPerson);
+    var newPerson = new MovieSpec(featureTitle, movieTime, userAge, quantity);
 
 
+    $(".receipt").show();
+    $("#filmTitle").text(newPerson.feature);
+    $("#filmTime").text(newPerson.movieTitle() );
+    $("#filmPrice").text(newPerson.price());
 
   });
 });
